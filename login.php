a8930e0b4c34cb80b13ae1f2cc64aa72fbcbdca7
寺下大典 <ekusiad.tera@icloud.com>
2022-05-05 11:02:12 +0900

    ログイン認証チェック

--- login.php
+++ login.php
@@ -1,15 +1,44 @@
 <?php
+$errmsg = array();
 if($_POST){
     //POST情報があるとき
     //1.入力チェック
+    if(!$_POST['e']){
+        $errmsg[] = 'Eメールを入力してください';
+    }elseif(strlen($_POST['e']) > 100){
+        $errmsg[] = 'Eメールは100文字以内で入力してください';
+    }
+
+    if(!$_POST['p']){
+        $errmsg[] = 'パスワードを入力してください';
+    }elseif(strlen($_POST['e']) > 50){
+        $errmsg[] = 'パスワードは50文字以内で入力してください';
+    }
+
     //2.ログインID,パスワードが一致しているかチェック
-    //3.ログイン後画面にリダイレクト
-    $host = $_SERVER['HTTP_HOST'];
-    $uri = rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
-    header("Location: //$host$uri/member.php");
-    exit;
+    $userfile = './user/userinfo.txt';
+    if(file_exists($userfile)){
+        $users = file_get_contents($userfile);
+        $users = explode("\n",$users);
+        foreach($users as $k => $v){
+            $v_ary = str_getcsv($v);
+            if($v_ary[0] == $POST['e']){
+                if(password_verify($_POST['p'],$v_ary[1])){
+                    $host = $_SERVER['HTTP_HOST'];
+                    $uri = rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
+                    header("Location: //$host$uri/member.php");
+                    exit;
+                }
+            }
+        }
+        $errmsg[] = 'ユーザーまたはパスワードが一致しません';
+    }else{
+        $errmsg[] = '見つかりません';
+    }
 }else{
     //GET情報のとき
+    $_POST = array();
+    $_POST['e'] = '';
 }
 ?>
 <!DOCTYPE html>
@@ -25,6 +54,9 @@ if($_POST){
 <body>
     <div class="container">
         <div class="mx-auto" style="width: 400px;">
+        <?php if($errmsg): ?>
+            <div class="alert alert-danger" role="alert"><?php echo implode('<br>',$errmsg); ?></div>
+        <?php endif; ?>
             <form action="./login.php" method="POST">
                 Eメール：<input type="email" name="e" value="" class="form-control"><br>
                 パスワード：<input type="password" name="p" value="" class="form-control"><br>
